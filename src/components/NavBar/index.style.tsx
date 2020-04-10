3b0e14836b55fba8e8687a5707c98dad09f38028
win <winhtaikaung28@gmail.com>
2020-04-11 00:20:09 +0630

    nav bar fixed

--- src/components/NavBar/index.style.tsx
+++ src/components/NavBar/index.style.tsx
@@ -4,6 +4,11 @@ export const NavBar = styled.div`
   background-color: rgb(187, 54, 46);
   height: 3.5rem;
   width: 100%;
+  position: absolute;
+  z-index: 1;
+  @media screen and (min-width: 64em) {
+    width: 50%;
+  }
 `;
 export const NavBarTitle = styled.p`
   color: white;
